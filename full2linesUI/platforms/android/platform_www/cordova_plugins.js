45a0d657461e73bbc999f48a767849e5c9785858
arai.tadaharu <arai.tadaharu@kk.jp.panasonic.com>
2018-06-08 19:04:59 +0900

    ios-platform、BLE Central、compat 追加

--- full2linesUI/platforms/android/platform_www/cordova_plugins.js
+++ full2linesUI/platforms/android/platform_www/cordova_plugins.js
@@ -1,8 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "cordova-plugin-ble-central.ble",
+        "file": "plugins/cordova-plugin-ble-central/www/ble.js",
+        "pluginId": "cordova-plugin-ble-central",
+        "clobbers": [
+            "ble"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-ble-central": "1.2.0",
     "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
