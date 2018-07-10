0efccea2d0291cc584ac741f99aa4d1682f189c8
okutani <okutani.shigeki@kk.jp.panasonic.com>
2018-07-10 11:38:26 +0900

    style 調整

--- full2line/src/components/channel/channel.js
+++ full2line/src/components/channel/channel.js
@@ -8,12 +8,10 @@ export default {
       tips: u.translate('channel.tips.text'),
       dots: {
         textAlign: 'center',
-        fontSize: '180%',
+        fontSize: '10vw',
         color: '#0076ff',
         position: 'absolute',
-        bottom: '0%',
-        left: 0,
-        right: 0,
+        bottom: '0vh',
       },
       modalVisible: false,
     };
