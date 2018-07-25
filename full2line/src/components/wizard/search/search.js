4cf9a47834cab4233d8f86d7cfb055b140794240
okutani <okutani.shigeki@kk.jp.panasonic.com>
2018-07-25 17:34:59 +0900

    mock stub bluetooth connect, disconnect

--- full2line/src/components/wizard/search/search.js
+++ full2line/src/components/wizard/search/search.js
@@ -4,12 +4,36 @@ export default {
   data() {
     return {
       isTips: false,
+      current: {},
+      list: [
+        {
+          id: 1,
+          name: 'first interface',
+          macAddress: '12-34-56-78',
+        },
+        {
+          id: 2,
+          name: 'second interface',
+          macAddress: '34-56-78-12',
+        },
+        {
+          id: 3,
+          name: 'third interface',
+          macAddress: '56-78-12-34',
+        },
+      ],
     };
   },
   methods: {
     translate: u.translate,
     close() {
       this.$emit('pop-page');
+    },
+    connect(id) {
+      this.current = u.find(this.list, { id });
+      u.blue.connect(this.current)
+      .then(() => this.isTips = true)
+      .catch(e => u.logger.error(e.message));
     },
   },
 };
--- full2line/src/modules/bluetooth/index.js
+++ full2line/src/modules/bluetooth/index.js
@@ -6,11 +6,37 @@ export class Bluetooth extends Base {
     super();
     u.logger.info('start', 'Bluetooth');
     this.status = {
-      power: 5,
+      active: false,
+      power: -1,
     };
   }
   initialize() {
-    setInterval(() => this.status.power ? this.status.power-- : this.status.power = 5, 3000);
+    setInterval(() => this.status.power === -1 ? undefined : this.getPower(), 3000);
+  }
+  getPower() {
+    // TODO: mock stub
+    return this.status.power ? this.status.power-- : this.status.power = 5;
+  }
+  connect(param) {
+    const promise = u.promise();
+    const cb = () => { // TODO: mock stub
+      u.merge(this.status, param);
+      this.status.active = true;
+      this.status.power = 5;
+      promise.resolve('ok');
+    };
+    setTimeout(cb.bind(this), 200);
+    return promise.instance;
+  }
+  disconnect() {
+    const promise = u.promise();
+    const cb = () => { // TODO: mock stub
+      this.status.active = false;
+      this.status.power = -1;
+      promise.resolve('ok');
+    };
+    setTimeout(cb.bind(this), 200);
+    return promise.instance;
   }
 }
 export default {
